c677f1618b058966094af6d31fd928a3e5b70d81
Martin Troup <martin.troup@etnetera.eu>
2021-09-17 19:39:13 +0200

    OvertureOperators set as a global dependency.

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .package(url: "https://github.com/CombineCommunity/CombineExt.git", from: "1.0.0"),
         .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "0.4.0"),
         .package(name: "Overture", url: "https://github.com/pointfreeco/swift-overture.git", from: "0.5.0"),
-        .package(name: "overture-operators", path: "../OvertureOperators")
+        .package(name: "overture-operators", url: "https://github.com/Qase/swift-overture-operators.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
