e218298fce71438e361cea1c2870b54fd4dd818a
Alžbeta Gogoláková <alzbeta.gogolakova@quanti.cz>
2021-12-01 17:08:56 +0100

    REF: replaces NSObject value(forKey:) with UserDefaults function object(forKey:)

--- Sources/Core/KeyValueStorage/UserDefaults/UserDefaults+.swift
+++ Sources/Core/KeyValueStorage/UserDefaults/UserDefaults+.swift
@@ -11,6 +11,10 @@ public extension UserDefaults {
     func store<Value>(value: Value, forKey key: String) {
         set(value, forKey: key)
     }
+    
+    func value<Value>(forKey key: String) -> Value? {
+        object(forKey: key) as? Value
+    }
 
     func delete(forKey key: String) {
         set(nil, forKey: key)
