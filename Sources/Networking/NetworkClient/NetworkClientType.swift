16d7884337cc7a2a1a327788074f5c59c01ece72
misicade <misicakova.a@gmail.com>
2023-02-02 15:21:57 +0100

    NetworkClientType async request functions added.

--- Sources/Networking/NetworkClient/NetworkClientType.swift
+++ Sources/Networking/NetworkClient/NetworkClientType.swift
@@ -16,7 +16,7 @@ public extension NetworkClientType {
     request(urlRequest)
       .decode(type: T.self, decoder: jsonDecoder, mapError: NetworkError.jsonDecodingError)
   }
-
+  
   func request<T: Decodable>(
     _ urlRequest: URLRequest,
     jsonDecoder: JSONDecoder = JSONDecoder(),
@@ -24,7 +24,7 @@ public extension NetworkClientType {
   ) -> AnyPublisher<(headers: [HTTPHeader], object: T), NetworkError> {
     request(urlRequest, jsonDecoder: jsonDecoder)
   }
-
+  
   func request<T: Decodable>(
     _ urlRequest: URLRequest,
     jsonDecoder: JSONDecoder = JSONDecoder()
@@ -33,7 +33,7 @@ public extension NetworkClientType {
       .map(\.object)
       .eraseToAnyPublisher()
   }
-
+  
   func request<T: Decodable>(
     _ urlRequest: URLRequest,
     jsonDecoder: JSONDecoder = JSONDecoder(),
@@ -41,10 +41,53 @@ public extension NetworkClientType {
   ) -> AnyPublisher<T, NetworkError> {
     request(urlRequest, jsonDecoder: jsonDecoder)
   }
-
+  
   func request(_ urlRequest: URLRequest) -> AnyPublisher<Void, NetworkError> {
     request(urlRequest)
       .map { _, _ in () }
       .eraseToAnyPublisher()
+  }
+}
+
+// MARK: - Synthetized async await functions
+
+public extension NetworkClientType {
+  func request<T: Decodable>(
+    _ urlRequest: URLRequest,
+    jsonDecoder: JSONDecoder = JSONDecoder()
+  ) async throws -> (headers: [HTTPHeader], object: T) {
+    try await request(urlRequest, jsonDecoder: jsonDecoder)
+      .async()
+  }
+  
+  func request<T: Decodable>(
+    _ urlRequest: URLRequest,
+    jsonDecoder: JSONDecoder = JSONDecoder(),
+    ofResponseType: T.Type
+  ) async throws -> (headers: [HTTPHeader], object: T) {
+    try await request(urlRequest, jsonDecoder: jsonDecoder, ofResponseType: ofResponseType)
+      .async()
+  }
+  
+  func request<T: Decodable>(
+    _ urlRequest: URLRequest,
+    jsonDecoder: JSONDecoder = JSONDecoder()
+  ) async throws -> T {
+    try await request(urlRequest, jsonDecoder: jsonDecoder)
+      .async()
+  }
+  
+  func request<T: Decodable>(
+    _ urlRequest: URLRequest,
+    jsonDecoder: JSONDecoder = JSONDecoder(),
+    ofResponseType: T.Type
+  ) async throws -> T {
+    try await request(urlRequest, jsonDecoder: jsonDecoder, ofResponseType: ofResponseType)
+      .async()
+  }
+  
+  func request(_ urlRequest: URLRequest) async throws -> Void {
+    _ = try await request(urlRequest)
+      .async()
   }
 }
