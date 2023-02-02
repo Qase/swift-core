6ee751df271cd12aa44301bb4a9a750e19d802aa
misicade <misicakova.a@gmail.com>
2023-02-02 15:19:08 +0100

    AnyPublisher async extension added.

---
+++ Sources/CombineExtensions/AnyPublisher+async.swift
@@ -0,0 +1,32 @@
+import Combine
+import Foundation
+
+public enum AsyncError: Error {
+  case finishedWithoutValue
+}
+
+public extension AnyPublisher {
+  func async() async throws -> Output {
+    try await withCheckedThrowingContinuation { continuation in
+      var cancellable: AnyCancellable?
+      var finishedWithoutValue = true
+      
+      cancellable = self.first()
+        .sink { result in
+          switch result {
+          case .finished:
+            if finishedWithoutValue {
+              continuation.resume(throwing: AsyncError.finishedWithoutValue)
+            }
+            
+          case let .failure(error):
+            continuation.resume(throwing: error)
+          }
+          cancellable?.cancel()
+        } receiveValue: { value in
+          finishedWithoutValue = false
+          continuation.resume(with: .success(value))
+        }
+    }
+  }
+}
